5983f1cc10e1d1c84d3a850543bf5d74db462380
gauravsngarg <gauravsngarg@gmail.com>
2016-05-16 23:41:53 +0530

    Added onTouchListener for Image, Pinch Zoom, Dragging

--- app/src/main/java/com/gauravsngarg/coloring/ui/ColoringPage.java
+++ app/src/main/java/com/gauravsngarg/coloring/ui/ColoringPage.java
@@ -1,15 +1,21 @@
 package com.gauravsngarg.coloring.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.gauravsngarg.coloring.R;
+import com.gauravsngarg.coloring.model.TouchImageView;
 
 public class ColoringPage extends AppCompatActivity {
 
+    private TouchImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coloring_page);
+
+        image = (TouchImageView) findViewById(R.id.img);
+
     }
+
 }
